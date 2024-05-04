bb1245d1f626feec2817651110e3166b76388d66
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2024-05-05 01:32:53 +0200

    добавил usecase в контроллер

--- src/features/games/quiz/self/GamesModule.ts
+++ src/features/games/quiz/self/GamesModule.ts
@@ -7,17 +7,12 @@ import { UsersRepoModule } from "../../../users/repo/UsersRepoModule";
 import { GamesPairGameQuizController } from "./controller/GamesPairGameQuizController";
 import { QuizGameQuestionsInGameModule } from "../questions.in.game/repo/game.quiz.questions.in.game.repo.module";
 import { GameQuizAnswersRepoModule } from "../answers/repo/game.quiz.answers.repo.module";
+import { GameQuizGetByIdUseCase } from "./service/use-cases/game.quiz.get.by.id.usecase";
 
-export const QuizGameUseCases = [GameQuizGetMyCurrentUseCase, QuizGameConnectToGameUseCase];
+export const QuizGameUseCases = [GameQuizGetMyCurrentUseCase, QuizGameConnectToGameUseCase, GameQuizGetByIdUseCase];
 
 @Module({
-  imports: [
-    GameQuizRepoModule,
-    GameQuizAnswersRepoModule,
-    UsersRepoModule,
-    QuizGameQuestionsInGameModule,
-    CqrsModule,
-  ],
+  imports: [GameQuizRepoModule, GameQuizAnswersRepoModule, UsersRepoModule, QuizGameQuestionsInGameModule, CqrsModule],
   controllers: [GamesPairGameQuizController],
   providers: [...QuizGameUseCases],
   exports: [...QuizGameUseCases],
--- src/features/games/quiz/self/controller/GamesPairGameQuizController.ts
+++ src/features/games/quiz/self/controller/GamesPairGameQuizController.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { CommandBus } from "@nestjs/cqrs";
 import { GamesRepoEntity } from "../repo/entities/GamesRepoEntity";
 import { QuizGameInfo } from "./entities/QuizGameGetMyCurrent/QuizGameGetMyCurrentUsecaseEntity";
@@ -7,6 +7,8 @@ import { JwtServiceUserAccessTokenLoad } from "../../../../../jwt/entities/JwtSe
 import { ReadAccessToken } from "../../../../../jwt/decorators/JwtRequestReadAccessToken";
 import { GameQuizGetMyCurrentCommand } from "../service/use-cases/game.quiz.get.my.current.usecase";
 import { QuizGameConnectToGameCommand } from "../service/use-cases/game.quiz.connection.usecase";
+import { GameQuizGetByIdCommand } from "../service/use-cases/game.quiz.get.by.id.usecase";
+import { QuizGameComplexInfo } from "../service/entities/quiz.game.complex.info";
 
 @Controller("pair-game-quiz")
 export class GamesPairGameQuizController {
@@ -14,9 +16,15 @@ export class GamesPairGameQuizController {
   @Get("pairs/my-current")
   @UseGuards(JwtAuthGuard)
   public async GetCurrentUserGame(@ReadAccessToken() tokenLoad: JwtServiceUserAccessTokenLoad) {
-    let game = await this.commandBus.execute<GameQuizGetMyCurrentCommand, GamesRepoEntity>(
-      new GameQuizGetMyCurrentCommand(tokenLoad.id),
-    );
+    let game = await this.commandBus.execute<GameQuizGetMyCurrentCommand, GamesRepoEntity>(new GameQuizGetMyCurrentCommand(tokenLoad.id));
+
+    return game;
+  }
+
+  @Get("pairs/:id")
+  @UseGuards(JwtAuthGuard)
+  public async GetById(@ReadAccessToken() tokenLoad: JwtServiceUserAccessTokenLoad, @Param("id") id: string) {
+    let game = await this.commandBus.execute<GameQuizGetByIdCommand, QuizGameComplexInfo>(new GameQuizGetByIdCommand(id, tokenLoad.id));
 
     return game;
   }
