ba947b0a521435e03dfabc6db891d04ee0ccdb37
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2024-05-16 02:50:43 +0200

    очередная правка алгоритма extra очков

--- src/features/games/quiz/rules/game.quiz.rules.ts
+++ src/features/games/quiz/rules/game.quiz.rules.ts
@@ -14,19 +14,19 @@ export class GameQuizRules {
   ): { p1_points: number; p2_points: number } {
     let res = { p1_points: 0, p2_points: 0 };
 
-    if (Object.keys(results[-1]).includes("p1_answer")) {
-      results[-1]["p2_answer"] = lastAnswer;
-      results[-1]["p2_answer_time"] = lastAnswerTime;
-    } else {
-      results[-1]["p1_answer"] = lastAnswer;
-      results[-1]["p1_answer_time"] = lastAnswerTime;
-    }
+    let p1_correctAnswerCount = results.filter((res) => res.answer.includes(res.p1_answer)).length;
+    let p2_correctAnswerCount = results.filter((res) => res.answer.includes(res.p2_answer)).length;
 
     let p1_endTime = results[-1].p1_answer_time;
     let p2_endTime = results[-1].p2_answer_time;
 
-    let p1_correctAnswerCount = results.filter((res) => res.answer.includes(res.p1_answer)).length;
-    let p2_correctAnswerCount = results.filter((res) => res.answer.includes(res.p2_answer)).length;
+    if (results[-1].p1_answer) {
+      p2_correctAnswerCount += results[-1].answer.includes(lastAnswer) ? 1 : 0;
+      p2_endTime = lastAnswerTime;
+    } else {
+      p1_correctAnswerCount += results[-1].answer.includes(lastAnswer) ? 1 : 0;
+      p1_endTime = lastAnswerTime;
+    }
 
     if (p1_endTime < p2_endTime && p1_correctAnswerCount > 0) res.p1_points = 1;
     else if (p2_endTime < p1_endTime && p2_correctAnswerCount > 0) res.p2_points = 1;
