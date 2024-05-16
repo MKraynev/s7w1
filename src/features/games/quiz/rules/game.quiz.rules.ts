3fb12ae3b1950e033aa6b01734d1187223fe62a8
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2024-05-16 02:02:19 +0200

    добавил механику экстра очков

--- src/features/games/quiz/rules/game.quiz.rules.ts
+++ src/features/games/quiz/rules/game.quiz.rules.ts
@@ -7,6 +7,19 @@ export class GameQuizRules {
     return 0;
   }
 
+  public static FinishExtraPoints(results: QuizGameQuestionsExtendedInfoEntity[]): { p1_points: number; p2_points: number } {
+    let res = { p1_points: 0, p2_points: 0 };
+
+    let p1_endTime = results[-1].p1_answer_time;
+    let p2_endTime = results[-1].p2_answer_time;
+    let p1_correctAnswerCount = results.filter((res) => res.answer.includes(res.p1_answer)).length;
+    let p2_correctAnswerCount = results.filter((res) => res.answer.includes(res.p2_answer)).length;
+
+    if (p1_endTime < p2_endTime && p1_correctAnswerCount > 0) res.p1_points = 1;
+    else if (p2_endTime < p1_endTime && p2_correctAnswerCount > 0) res.p2_points = 1;
+
+    return res;
+  }
   public static GetGameQuestionCount() {
     return 5;
   }
--- src/features/games/quiz/self/service/use-cases/game.quiz.answer.the.question.usecase.ts
+++ src/features/games/quiz/self/service/use-cases/game.quiz.answer.the.question.usecase.ts
@@ -64,6 +64,18 @@ export class GameQuizAnswerTheQuestionUseCase implements ICommandHandler<GameQui
       userGame.status = "Finished";
       userGame.endedAt = savedAnswer.createdAt;
 
+      if (userIsFirstPlayer) {
+        gameQuestionsAndAnswersInfo[-1].p1_answer = savedAnswer.answer;
+        gameQuestionsAndAnswersInfo[-1].p1_answer_time = savedAnswer.createdAt;
+      } else {
+        gameQuestionsAndAnswersInfo[-1].p2_answer = savedAnswer.answer;
+        gameQuestionsAndAnswersInfo[-1].p2_answer_time = savedAnswer.createdAt;
+      }
+
+      let { p1_points, p2_points } = GameQuizRules.FinishExtraPoints(gameQuestionsAndAnswersInfo);
+      userGame.player_1_score += p1_points;
+      userGame.player_2_score += p2_points;
+
       console.log("both users answered right now");
     }
 
