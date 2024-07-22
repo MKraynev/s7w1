6b4804083411a05dea8dd23f7b5a05501e838a1c
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2024-07-22 21:51:19 +0200

    поправил import blogger controller

--- src/features/blogger/blogger.module.ts
+++ src/features/blogger/blogger.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserToBlogRepoEntity } from "./repo/entities/user.to.blog.repo.entity";
 import { BloggerPostBlogUseCase } from "./service/use-cases/blogger.post.blog.usecase";
+import { BloggerController } from "./controller/blogger.controller";
 
 const BloggerUseCases = [BloggerPostBlogUseCase];
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserToBlogRepoEntity])],
-  controllers: [],
+  controllers: [BloggerController],
   providers: [...BloggerUseCases],
   exports: [...BloggerUseCases],
 })
