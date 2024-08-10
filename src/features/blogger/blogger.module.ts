468c2a89faa8c65de37c55007563f2b66e7f785e
Матвей Крайнев <matvejkrajnev@iMac-Matvej.local>
2024-08-10 18:58:52 +0200

    debug delete

--- src/features/blogger/blogger.module.ts
+++ src/features/blogger/blogger.module.ts
@@ -5,8 +5,17 @@ import { BloggerPostBlogUseCase } from "./service/use-cases/blogger.post.blog.us
 import { BloggerController } from "./controller/blogger.controller";
 import { CqrsModule } from "@nestjs/cqrs";
 import { BloggerPutBlogsByIdUseCase } from "./service/use-cases/blogger.put.blogs.by.id.usecase";
+import { BloggerDeleteBlogsByIdUseCase } from "./service/use-cases/blogger.delete.blogs.by.id.usecase";
+import { BloggerGetBlogsUseCase } from "./service/use-cases/blogger.get.blogs.usecase";
+import { BloggerPostPostByBlogIdUseCase } from "./service/use-cases/blogger.post.post.by.blog.id.usecase";
 
-const BloggerUseCases = [BloggerPostBlogUseCase, BloggerPutBlogsByIdUseCase];
+const BloggerUseCases = [
+  BloggerPostBlogUseCase,
+  BloggerPutBlogsByIdUseCase,
+  BloggerDeleteBlogsByIdUseCase,
+  BloggerGetBlogsUseCase,
+  BloggerPostPostByBlogIdUseCase,
+];
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserToBlogRepoEntity]), CqrsModule],
